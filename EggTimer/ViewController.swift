82417426eb621d5cc9cda615995f0863ba98357e
rajtailor96 <rajtailor96@gmail.com>
2021-02-11 20:50:14 -0600

    Added Counter

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,18 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    
+    var secondsPassed = 0
+    var timer = Timer()
+    var totalTime = 0
     let eggTimes = ["Soft": 5,
                     "Medium": 8,
                     "Hard": 12]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        totalTime = eggTimes[sender.currentTitle!]!
         
-        print(eggTimes[hardness!]!)
+        timerProgress.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = sender.currentTitle!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
     }
     
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let increment = Float(secondsPassed)/Float(totalTime)
+            
+            timerProgress.setProgress(increment, animated: true)
+        } else {
+            timer.invalidate()
+
+            titleLabel.text = "Done!"
+        }
+    }
+    
+    @IBAction func musicSwitch(_ sender: UISwitch) {
+        if(sender.isOn){
+            MP3.background.startMusic()
+        } else {
+            MP3.background.stopMusic()
+        }
+        
+        
+    }
+}
+
+class MP3{
+    static let background = MP3()
+    var audioPlayer: AVAudioPlayer?
+    
+    func startMusic(){
+        if let bundle = Bundle.main.path(forResource: "backgroundMusic", ofType: "mp3"){
+            let backgroundMusic = NSURL(fileURLWithPath: bundle)
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: backgroundMusic as URL)
+                guard let audioPlayer = audioPlayer else {return}
+                audioPlayer.numberOfLoops = -1
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    func stopMusic() {
+        audioPlayer?.stop()
+    }
 }
